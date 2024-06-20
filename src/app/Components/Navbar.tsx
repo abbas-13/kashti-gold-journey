db78353a14e6cc61b66f6d06e8aebfe40fbdf61e
mohammad abbas <abbasathe61@astronaut-2.local>
2024-06-21 04:13:02 +0530

    initial commit

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Logo from "public/logo.png";
+
+export const Navbar = () => {
+  return (
+    <div className="w-full h-14 shadow flex items-center">
+      <div className="flex items-center gap-2 ml-24">
+        <Image alt="Logo" src={Logo} width={22} />
+        <p className="font-brooklyn font-medium text-[#2b3980]">KASHTI</p>
+      </div>
+    </div>
+  );
+};
